36f5933c98bd9519bd67b4458465aee846faddc7
miwnek <mwnek@student.agh.edu.pl>
2022-10-17 17:57:02 +0200

    lab3

---
+++ src/main/java/agh/ics/oop/Animal.java
@@ -0,0 +1,26 @@
+package agh.ics.oop;
+
+
+public class Animal {
+    private MapDirection currDirection = MapDirection.NORTH;
+    private Vector2d currPosition = new Vector2d(2, 2);
+
+    public String toString() {
+        return String.join("", currPosition.toString(), " ", currDirection.toString());
+    }
+
+    public boolean isAt(Vector2d position) {
+        return (currPosition.equals(position));
+    }
+
+    public void move(MoveDirection direction) {
+        switch (direction) {
+            case RIGHT -> currDirection = currDirection.next();
+            case LEFT -> currDirection = currDirection.previous();
+            case FORWARD -> currPosition = currPosition.add(currDirection.toUnitVector());
+            case BACKWARD -> currPosition = currPosition.subtract(currDirection.toUnitVector());
+        }
+        currPosition = currPosition.lowerLeft(new Vector2d(4, 4));
+        currPosition = currPosition.upperRight(new Vector2d(0, 0));
+    }
+}
---
+++ src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,23 @@
+package agh.ics.oop;
+
+public class OptionsParser {
+    public static MoveDirection[] parse(String[] args) {
+        int count = 0;
+        for (String str : args) {
+            if(str.equals("f") || str.equals("forward") || str.equals("b")
+                    || str.equals("backward") || str.equals("r") || str.equals("right")
+                    || str.equals("l") || str.equals("left")) count++;
+        }
+        MoveDirection[] newArgs = new MoveDirection[count];
+        int newCount = 0;
+        for(int i = 0; i < count; i++){
+            switch(args[i]){
+                case "f", "forward" -> newArgs[newCount++] = MoveDirection.FORWARD;
+                case "b", "backward" -> newArgs[newCount++] = MoveDirection.BACKWARD;
+                case "r", "right" -> newArgs[newCount++] = MoveDirection.RIGHT;
+                case "l", "left" -> newArgs[newCount++] = MoveDirection.LEFT;
+            }
+        }
+        return newArgs;
+    }
+}
